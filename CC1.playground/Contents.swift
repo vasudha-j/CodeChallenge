89d439646a017219f27b04d2a39e9e7ed7634cd7
Vasudha Jags <jagarlamudy1991@gmail.com>
2017-08-24 06:07:44 -0500

    CodeChallenge Day 1 in Swift

---
+++ CC1.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+
+// Declare second integer, double, and String variables.
+var secondIntVariable : Int
+var secondDoubleVariable : Double
+var secondStringVariable : String
+// Read and save an integer, double, and String to your variables.
+secondIntVariable = 12
+secondDoubleVariable = 4.0
+secondStringVariable = "is the best place to learn and practice coding!"
+
+// Print the sum of both integer variables on a new line.
+print(i + secondIntVariable)
+
+// Print the sum of the double variables on a new line.
+print(d + secondDoubleVariable)
+
+// Concatenate and print the String variables on a new line
+print(s + secondStringVariable)
+// The 's' variable above should be printed first.
